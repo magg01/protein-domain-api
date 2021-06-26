c1b7cc0e99648ec9e5125bb34171ad98e5fcdb3b
Matthew Maggiolini <magg01@gmail.com>
2021-06-26 17:59:56 +0200

    api_view implemented for protein

--- coursework/proteindomains/restAPI/api.py
+++ coursework/proteindomains/restAPI/api.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import *
 from .serializers import *
 
-@csrf_exempt
+@api_view(['GET','POST'])
 def protein_detail(request, pk):
     try:
         protein = Protein.objects.get(pk=pk)
@@ -12,4 +15,4 @@ def protein_detail(request, pk):
         return HttpResponse(status=404)
     if request.method == 'GET':
         serializer = ProteinSerializer(protein)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
